bf6f91e9667db5a6116c4f8fbc6d6a7e0eae8fb0
Steven Laczko <stevendlaczko@gmail.com>
2022-05-09 19:52:49 -0400

    added vim install

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ done
 
 source ./config.sh
 
+$curdir=$(pwd)
+
 cp ./.bashrc $HOME
 source $HOME/.bashrc
 sudo cp ./rundwm /usr/local/bin
@@ -19,20 +21,27 @@ sudo apt-get install -f
 cp ./.tmux.conf $HOME
 echo "Tmux installed"
 
+# Network stuff
+echo -e "\n\nrename $wifi=wifi\nauto wifi\niface wifi inet dhcp\n\nrename $ethernet=eth\niface eth inet dhcp" >> /etc/network/interfaces
+sed -i 's/managed=false/managed=true/' /etc/NetworkManager/NetworkManager.conf
+
+echo -e "Network set up.\nSet up wifi with nmtui"
+
 # Install dwm
 sudo mkdir /src
 sudo git clone https://git.suckless.org/dwm /src/dwm
 git apply --directory=/src/dwm ./dwm-diff-file
 cd /src/dwm
 make
+cd $curdir
 
 # Enable dwm in x11
 touch ~/.xinitrc
 echo -e "\n\nexec /usr/local/bin/rundwm" >> ~/.xinitrc
 echo "dwm installed"
 
-# Network stuff
-echo -e "\n\nrename $wifi=wifi\nauto wifi\niface wifi inet dhcp\n\nrename $ethernet=eth\niface eth inet dhcp" >> /etc/network/interfaces
-sed -i 's/managed=false/managed=true/' /etc/NetworkManager/NetworkManager.conf
+# Install vim
+git clone git@github.com:StevenLaczko/vim-install.git
+sudo chmod +x ./vim-install/install.sh
+exec ./vim-install/install.sh
 
-echo -e "Network set up.\nSet up wifi with nmtui"
